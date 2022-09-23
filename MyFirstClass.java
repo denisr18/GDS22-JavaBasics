2a320c99c796e5a6a475e57a7464afb1aa6132b0
denis <52206534@edu.campus02.at>
2022-09-23 17:10:15 +0200

    Neue Klasse hinzugefügt; args als input parameter

---
+++ MyFirstClass.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+    //Wir haben unsere erste Klasse erzeugt
+    //psvm
+    public static void main(String[] args) {
+        //erste Ausgabe
+        System.out.println("Ich werde auf der Kosnole angezeigt");
+
+        //args
+        //sout + tab
+        System.out.println(Arrays.toString(args));
+    }
+}
